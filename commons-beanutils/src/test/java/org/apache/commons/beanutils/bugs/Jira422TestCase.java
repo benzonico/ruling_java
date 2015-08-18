a76f344b31af733e281fc1921531fd78f5cf40ee
Nicolas PERU <nicolas.peru@sonarsource.com>
2015-08-18 13:41:57 +0200

    SONARJAVA-1223 Add common-beanutils

---
+++ commons-beanutils/src/test/java/org/apache/commons/beanutils/bugs/Jira422TestCase.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.commons.beanutils.bugs;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.beanutils.PropertyUtils;
+
+/**
+ * getPropertyType return null on second descendant class
+ *
+ * @version $Id: Jira422TestCase.java 1632171 2014-10-15 20:15:17Z ggregory $
+ * @see <a href="https://issues.apache.org/jira/browse/BEANUTILS-422">https://issues.apache.org/jira/browse/BEANUTILS-422</a>
+ */
+public class Jira422TestCase extends TestCase {
+
+    public void testRootBean() throws Exception {
+        final RootBean bean = new FirstChildBean();
+        final Class<?> propertyType = PropertyUtils.getPropertyType(bean, "file[0]");
+        assertEquals(String.class.getName(), propertyType.getName());
+    }
+
+    public void testSecondChildBean() throws Exception {
+        final RootBean bean = new SecondChildBean();
+        final Class<?> propertyType = PropertyUtils.getPropertyType(bean, "file[0]");
+        assertEquals(String.class.getName(), propertyType.getName());
+    }
+
+}
+
+@SuppressWarnings("rawtypes")
+class RootBean {
+
+    private List file;
+
+    public List getFile() {
+        return file;
+    }
+
+    public void setFile(final List file) {
+        this.file = file;
+    }
+
+    public String getFile(final int i) {
+        return (String) file.get(i);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void setFile(final int i, final String file) {
+        this.file.set(i, file);
+    }
+
+}
+
+class FirstChildBean extends RootBean {
+}
+
+class SecondChildBean extends RootBean {
+}
---
+++ commons-beanutils/src/test/java/org/apache/commons/beanutils/converters/SqlTimestampConverterTestCase.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.beanutils.converters;
+
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Locale;
+
+import junit.framework.TestSuite;
+
+/**
+ * Test Case for the {@link SqlTimestampConverter} class.
+ *
+ * @version $Id: SqlTimestampConverterTestCase.java 1632171 2014-10-15 20:15:17Z ggregory $
+ */
+
+public class SqlTimestampConverterTestCase extends DateConverterTestBase {
+
+    /**
+     * Construct a new Date test case.
+     * @param name Test Name
+     */
+    public SqlTimestampConverterTestCase(final String name) {
+        super(name);
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Create Test Suite
+     * @return test suite
+     */
+    public static TestSuite suite() {
+        return new TestSuite(SqlTimestampConverterTestCase.class);
+    }
+
+    // ------------------------------------------------------------------------
+
+    /**
+     * Test Date Converter with no default value
+     */
+    @Override
+    public void testLocale() {
+
+        // Re-set the default Locale to Locale.US
+        final Locale defaultLocale = Locale.getDefault();
+        Locale.setDefault(Locale.US);
+
+        final String pattern = "M/d/yy h:mm a"; // SHORT style Date & Time format for US Locale
+
+        // Create & Configure the Converter
+        final DateTimeConverter converter = makeConverter();
+        converter.setUseLocaleFormat(true);
+
+        // Valid String --> Type Conversion
+        final String testString = "3/21/06 3:06 pm";
+        final Object expected = toType(testString, pattern, null);
+        validConversion(converter, expected, testString);
+
+        // Invalid Conversions
+        invalidConversion(converter, null);
+        invalidConversion(converter, "");
+        invalidConversion(converter, "13:05 pm");
+        invalidConversion(converter, "11:05 p");
+        invalidConversion(converter, "11.05 pm");
+        invalidConversion(converter, new Integer(2));
+
+        // Restore the default Locale
+        Locale.setDefault(defaultLocale);
+
+    }
+
+    /**
+     * Test default String to java.sql.Timestamp conversion
+     */
+    @Override
+    public void testDefaultStringToTypeConvert() {
+
+        // Create & Configure the Converter
+        final DateTimeConverter converter = makeConverter();
+        converter.setUseLocaleFormat(false);
+
+        // Valid String --> java.sql.Timestamp Conversion
+        final String testString = "2006-10-23 15:36:01.0";
+        final Object expected = toType(testString, "yyyy-MM-dd HH:mm:ss.S", null);
+        validConversion(converter, expected, testString);
+
+        // Invalid String --> java.sql.Timestamp Conversion
+        invalidConversion(converter, "2006/09/21 15:36:01.0");
+        invalidConversion(converter, "2006-10-22");
+        invalidConversion(converter, "15:36:01");
+
+    }
+
+    /**
+     * Create the Converter with no default value.
+     * @return A new Converter
+     */
+    @Override
+    protected DateTimeConverter makeConverter() {
+        return new SqlTimestampConverter();
+    }
+
+    /**
+     * Create the Converter with a default value.
+     * @param defaultValue The default value
+     * @return A new Converter
+     */
+    @Override
+    protected DateTimeConverter makeConverter(final Object defaultValue) {
+        return new SqlTimestampConverter(defaultValue);
+    }
+
+    /**
+     * Return the expected type
+     * @return The expected type
+     */
+    @Override
+    protected Class<?> getExpectedType() {
+        return Timestamp.class;
+    }
+
+    /**
+     * Convert from a Calendar to the appropriate Date type
+     *
+     * @param value The Calendar value to convert
+     * @return The converted value
+     */
+    @Override
+    protected Object toType(final Calendar value) {
+        return new Timestamp(getTimeInMillis(value));
+    }
+
+}
